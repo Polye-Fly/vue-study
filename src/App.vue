f5f3b380bff9204e11794c24cefa82c8fd93346b
linswin <Ac_successFly@163.com>
2023-11-24 18:06:08 +0800

    20231124

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,8 @@
   <EventDemo1/> -->
   <!-- <EventDemo2/> -->
   <!-- <EventDemo3/> -->
-  <EventDemo4/>
+  <!-- <EventDemo4/> -->
+  <EventDemo5/>  
 </template>
 
 <script setup>
@@ -28,6 +29,7 @@ import EventDemo1 from "./components/EventDemo1.vue"
 import EventDemo2 from "./components/EventDemo2.vue"
 import EventDemo3 from "./components/EventDemo3.vue"
 import EventDemo4 from "./components/EventDemo4.vue"
+import EventDemo5 from "./components/EventDemo5.vue"
 // export default{
 //   // 文本插值的语法
 //   // data(){
---
+++ src/components/EventDemo5.vue
@@ -0,0 +1,37 @@
+<template>
+    <h3>事件修饰符</h3>
+    <!-- 阻止默认事件还可以使用以下的形式：
+        @click.prevent -->
+    <a @click="clickHandle" href="https://www.baidu.com/">百度</a>
+    <!-- 阻止事件冒泡还可以使用以下的形式：
+        @click.stop -->
+    <div @click="clickDiv">
+        <p @click="clickP"></p>
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            names:["1","2","3"]
+        }
+    },
+    // 所有的方法都要写在这里
+    methods:{
+        clickHandle(e){
+            //阻止默认事件
+            e.preventDefault();
+            console.log("点击按钮")
+        },
+        clickDiv(e){
+            console.log("DIV")
+        },
+        clickP(e){
+            //阻止事件冒泡
+            e.stopPropagation()
+            console.log("p")
+        }
+    }
+}
+</script>
