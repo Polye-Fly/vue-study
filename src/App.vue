f88bef04380e0da2e497077de9e07831da851a5d
linswin <Ac_successFly@163.com>
2023-11-30 23:45:08 +0800

    vue 组件生命周期

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+
+  <h3>组件生命周期</h3>
+  <UserComponment/>
+</template>
+
+<script>
+/**
+ * 生命周期函数
+ *    创建期：beforeCreate created
+ *    挂载期：beforeMount mounted
+ *    更新期：beforeUpdate update
+ *    销毁期：beforeUmount umounted
+ */
+import UserComponment from './components/UserComponment.vue';
+export default{
+  data(){
+    return {
+
+    }
+  },
+  components:{
+    UserComponment
+  }
+}
+</script>
---
+++ src/components/UserComponment.vue
@@ -0,0 +1,18 @@
+<template>
+    <h3>组件生命周期函数的应用</h3>
+    <p ref="name">AAAAA</p>
+</template>
+
+<script>
+export default{
+    data(){
+
+    },
+    beforeMount(){
+        console.log(this.$refs.name)  //undefined
+    },
+    mounted(){
+        console.log(this.$refs.name)
+    }
+}
+</script>
